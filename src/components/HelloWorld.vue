5177d889079198d02e9f45877068d4e8b0aae0bb
CornerSkyless <573196853@qq.com>
2018-04-18 13:43:10 +0800

    首次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hanoi">
+    <div class="hanoi-main-area">
+      <hanoi-tower />
+      <hanoi-tower />
+      <hanoi-tower />
+    </div>
+  </div>
+</template>
+
+<script>
+  import HanoiTower from './Tower';
+  export default {
+    name: 'Hanoi',
+    data () {
+      return {
+      }
+    },
+    components:{ HanoiTower }
+  }
+</script>
+
+<style type="text/scss" lang="scss" >
+  @import '../assets/style.scss';
+</style>
---
+++ src/components/Tower.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hanoi-tower">
+    <div class="hanoi-tower-line"></div>
+    <div class="hanoi-tower-blocks">
+      <div class="hanoi-tower-block">1</div>
+      <div class="hanoi-tower-block">2</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HanoiTower',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
